4a430f20ef6e2dd327edd0adef75e31bde45cbc5
alxndrd <65692603+alxndrd@users.noreply.github.com>
2021-10-01 08:49:36 +0700

    Delete examples.rb

--- examples.rb
+++
@@ -1,7 +0,0 @@
-puts "Стоимость 1 доллара"
-dol = gets.chomp
-
-puts "Сколько перевести в рублях?"
-rub = gets.chomp
-resultDollar = rub.to_f / dol.to_f
-puts "У вас #{resultDollar.round(2)} долларов" #вывод результата с оркгулением до двух знаков round(2) 
